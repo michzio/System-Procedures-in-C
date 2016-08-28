f2c97feb0550298b5330d627d26504d86fa0a2b9
michzio <michzio@hotmail.com>
2016-08-28 12:31:19 +0200

    added new hash_map project

--- hash_map/hash_map.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by Michal Ziobro on 28/08/2016.
-//
-
-#include "hash_map.h"
-
--- hash_map/hash_map.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Michal Ziobro on 28/08/2016.
-//
-
-#ifndef HASH_MAP_HASH_MAP_H
-#define HASH_MAP_HASH_MAP_H
-
-#endif //HASH_MAP_HASH_MAP_H
