8bb3a7db2fdaffadce0ed3a32ecc3e4c100bf853
michzio <michzio@hotmail.com>
2016-10-05 14:20:40 +0200

    cocoa helper refactored image bytes array

--- OS_X/cocoa_helper/images.h
+++ OS_X/cocoa_helper/images.h
@@ -15,6 +15,15 @@
 
 bool CGImageSaveToFile(CGImageRef image, CFStringRef file_path, CFStringRef file_name, CFStringRef file_type);
 bool CGImageCopyToClipboard(CGImageRef image);
+
+unsigned char *CGImageCopyRawData(CGImageRef image, size_t *dataLength);
 unsigned char *CGImageCopyRGBAData(CGImageRef image, size_t *rgbaDataLength);
+unsigned char *CGImageCopyARGBData(CGImageRef image, size_t *argbDataLength);
+unsigned char *CGImageCopyCMYKData(CGImageRef image, size_t *cmykDataLength);
+CGImageRef CGImageCreateWithRawData(const unsigned char *rawData, const size_t rawDataLength, const size_t width, const size_t height, CGColorSpaceRef colorSpace);
+CGImageRef CGImageCreateWithRGBAData(const unsigned char *rgbaData, const size_t rgbaDataLength, const size_t imageWidth, const size_t imageHeight);
+CGImageRef CGImageCreateWithARGBData(const unsigned char *argbData, const size_t argbDataLength, const size_t width, const size_t height);
+CGImageRef CGImageCreateWithCMYKData(const unsigned char *cmykData, const size_t cmykDataLength, const size_t width, const size_t height);
+void CGImageDumpInfo(CGImageRef image, bool shouldPrintPixelData);
 
 #endif //COCOA_HELPER_IN_OS_X_IMAGES_H
